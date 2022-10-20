99f0a2c3cfb22dbee260419dbeac8e396e3f66b1
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-20 19:31:01 -0400

    adding files

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'top',
     'style_external_links': False,
     'vcs_pageview_mode': 'edit',
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '#E2E7FF',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
