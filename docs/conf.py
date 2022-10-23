672f93b7f163307d7dd15d6c66f2247491190b7f
Robin Mordasiewicz <robin@mordasiewicz.com>
2022-10-23 16:10:42 -0400

    adding files

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 #html_theme = 'sphinx_book_theme'
 #html_theme = "insipid"
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
